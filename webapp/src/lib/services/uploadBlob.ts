98ccfbf9d30d04ee8a694044b2e2ed49a7d660fe
Tyler Cako <>
2024-09-11 11:25:44 -0600

    began work on blob storage upload

--- webapp/src/lib/services/uploadBlob.ts
+++ webapp/src/lib/services/uploadBlob.ts
@@ -1,3 +1,14 @@
+import { DefaultAzureCredential } from '@azure/identity';
+import { BlobServiceClient } from '@azure/storage-blob';
+
+const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+if (!accountName) throw Error('Azure Storage accountName not found');
+
+const blobServiceClient = new BlobServiceClient(
+	`https://${accountName}.blob.core.windows.net`,
+	new DefaultAzureCredential()
+);
+
 export const uploadBlob = async (pokemon: string, blob: Blob) => {
 	const blobText = await blob.text();
 	localStorage.setItem(pokemon, blobText);
--- webapp/src/routes/training/+page.svelte
+++ webapp/src/routes/training/+page.svelte
@@ -3,21 +3,41 @@
 	import { onMount } from 'svelte';
 	import { uploadBlob } from '$lib/services/uploadBlob';
 	import type { PokemonJSON } from '$lib/types/PokemonJSON';
+	import { Button } from 'flowbite-svelte';
 
 	const pokemonArr = ['bulbasaur', 'charmander', 'squirtle'];
 	let currentPokemon = pokemonArr[0];
 	let pokemonJSON: PokemonJSON | null;
 
+	let counter = 0;
+
+	const updateCounter = async () => {
+		counter++;
+
+		if (counter == 10) {
+			currentPokemon = pokemonArr[1];
+			updatePokemon();
+		}
+		if (counter == 20) {
+			currentPokemon = pokemonArr[2];
+			updatePokemon();
+		}
+
+		console.log(counter);
+	};
+
+	const updatePokemon = async () => {
+		const pokemonReq = await fetch(`https://pokeapi.co/api/v2/pokemon/${currentPokemon}`);
+		pokemonJSON = await pokemonReq.json();
+	};
+
 	const uploadCanvas = (blob: Blob) => {
 		uploadBlob(currentPokemon, blob);
+		updateCounter();
 	};
 
 	onMount(async () => {
-		const pokemonReq = await fetch(`https://pokeapi.co/api/v2/pokemon/${currentPokemon}`);
-		console.log(pokemonReq);
-		pokemonJSON = await pokemonReq.json();
-		console.log(pokemonJSON);
-		console.log(typeof pokemonJSON);
+		updatePokemon();
 	});
 </script>
 
@@ -25,5 +45,10 @@
 	<div class="w-full flex items-center flex-col">
 		<h2>Training</h2>
 	</div>
-	<Draw {pokemonJSON} on:canvasSubmit={(e) => uploadCanvas(e.detail.blob)} />
+	{#if counter < 30}
+		<Draw {pokemonJSON} on:canvasSubmit={(e) => uploadCanvas(e.detail.blob)} />
+	{:else}
+		<div>All done!</div>
+		<Button data-sveltekit-reload href="/training">Train again</Button>
+	{/if}
 </div>
