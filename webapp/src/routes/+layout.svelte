927e3d70a7db01f3430fe6b912afda00d062184a
Tyler Cako <>
2025-03-14 02:13:19 -0600

    Fixed background image and woorking on deploying model

--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script>
 	import '../app.css';
+	import img from '$lib/assets/bg.jpg'
+
+	$: bgImage = `background-image: url("${img}");`;
 
 	$: innerWidth = 0;
 </script>
 
 <svelte:window bind:innerWidth />
 
-<div class="min-h-screen min-w-fit bg-[url(bg.jpg)] bg-cover bg-[position:center]">
+<div style = {bgImage} class="min-h-screen min-w-fit bg-cover bg-[position:center]">
 	{#if innerWidth >= 768}
 		<div class="flex align-center justify-center h-full">
 			<div class="w-9/12 h-full">
